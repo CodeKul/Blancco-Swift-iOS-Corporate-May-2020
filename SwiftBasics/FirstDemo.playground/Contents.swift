a2b1188775f9a4d25d45f5ae6529eb1b8da65141
Varun Kudalkar <varun.kudalkar@gmail.com>
2020-05-05 10:17:15 +0530

    first demo

---
+++ SwiftBasics/FirstDemo.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var name = "Codekul"
+name = "Swift"
+name
+
+//name = 10
+
+var var1 = 10
+var var2 = 12.34
+var var3 = false
+
+var a = 10
+var b = 20
+
+print(a)
+
+let const1 = "Swift"
+//const1 = "Objective C"
+
+
+var var4: String
+var4 = "iOS"
+print(var4)
+
+var var5: Int
+var5 = 11
+
+var var6: Double
+//var6 = var5
+var6 = 11
+print(var6)
+
+
+var 🐶 = 10
+print(🐶)
+🐶
+
+var 🐱 = 20
+print(🐱)
+
+print(🐶 + 🐱)
